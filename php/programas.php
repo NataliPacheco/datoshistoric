cf27a478c6aca65225f91ab097867b314db0eddf
NataliPacheco <lnatali_pacheco@hotmail.com>
2017-12-28 15:56:14 -0600

    Agregue consulta inner joins

--- php/programas.php
+++ php/programas.php
@@ -2,7 +2,15 @@
    //Establecer conexion con la base de datos
   include("conexion.php");
   $json=array();
-  $consulta=mysql_query("SELECT programas.num_referencia,programas.nombre_programa, cat_convocatoria.nombre_convocatoria FROM cat_convocatoria inner join programas on programas.id_convocatoria = cat_convocatoria.id_convocatoria",$con) or die ("No se puede realizar la consulta");
+  $consulta=mysql_query("SELECT programas.id_programa,programas.num_referencia,programas.nombre_programa,cat_convocatoria.nombre_convocatoria,
+       cat_evaluadores.nombre_evaluador,cat_evaluadores.cvu_evaluador, comite.fecha_comite,cat_dictamen.estatus,
+       cat_nivel.nombre_nivel,cat_institucion.nombre_institucion FROM programas 
+  	inner join cat_convocatoria  on programas.id_convocatoria = cat_convocatoria.id_convocatoria
+  	inner join cat_institucion on programas.id_institucion = cat_institucion.id_institucion
+    inner join cat_evaluadores on programas.cvu_evaluador = cat_evaluadores.cvu_evaluador
+    inner join comite on programas.id_comite = comite.id_comite
+    inner join cat_dictamen on programas.id_dictamen = cat_dictamen.id_dictamen
+    inner join cat_nivel on programas.id_nivel = cat_nivel.id_nivel order by id_programa",$con) or die ("No se puede realizar la consulta");
 
   while($row=mysql_fetch_assoc($consulta)) {
 
