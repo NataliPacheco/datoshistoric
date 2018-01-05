207a8a203c1db9ebb2ca0bd69e66922aad68c675
NataliPacheco <lnatali_pacheco@hotmail.com>
2018-01-05 16:54:16 -0600

    agregue cambios

--- js/programas.js
+++ js/programas.js
@@ -1,8 +1,23 @@
- $.get("programas.php")
-    .success(function(data) {
-    	$("#tprogramas").append(data)
-    })
-    .error(function(data){
-        console.log(data);
-   
+ $.get("php/programas.php",function(data) {
+    	//console.log(data);
+    	data=JSON.parse(data);
+    	var datos="";
+    	data.forEach(function(index,valor){
+    		datos="<tr>";
+    			datos += "<td>"+index.num_referencia+"</td>"
+    			datos += "<td>"+index.nombre_programa+"</td>"
+    			datos += "<td>"+index.nombre_convocatoria+"</td>"
+    			datos += "<td>"+index.nombre_evaluador+"</td>"
+    			datos += "<td>"+index.cvu_evaluador+"</td>"
+    			datos += "<td>"+index.fecha_comite+"</td>"
+    			///datos += "<td>"+index.nombre_institucion+"</td>"
+    			datos += "<td>"+index.estatus+"</td>"
+    			datos += "<td>"+index.nombre_nivel+"</td>"
+
+    		datos+="</tr>";
+    		$("#resultados").append(datos)
+    		
+    	});
+    		console.log(datos);
+    	//$("#").append(data)
     });
