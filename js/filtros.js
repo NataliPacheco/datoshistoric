9a3b73a780aa322bac00e8a228054995a23a9ad5
NataliPacheco <lnatali_pacheco@hotmail.com>
2018-01-07 16:52:36 -0600

    cambios en js filtros php filtros y resultados

--- js/filtros.js
+++ js/filtros.js
@@ -18,11 +18,28 @@ $("#buscar").click(function() {
 		nivel:$("#nivel").val(),
 		institucion:$("#institucion").val(),
 
+	},
+	success: function(data) {
+		$("#resultados").empty();
+		data=JSON.parse(data);
+    	var datos="";
+    	data.forEach(function(index,valor){
+    		datos="<tr>";
+    			datos += "<td>"+index.num_referencia+"</td>"
+    			datos += "<td>"+index.nombre_programa+"</td>"
+    			datos += "<td>"+index.nombre_convocatoria+"</td>"
+    			datos += "<td>"+index.nombre_evaluador+"</td>"
+    			datos += "<td>"+index.cvu_evaluador+"</td>"
+    			datos += "<td>"+index.fecha_comite+"</td>"
+    			///datos += "<td>"+index.nombre_institucion+"</td>"
+    			datos += "<td>"+index.estatus+"</td>"
+    			datos += "<td>"+index.nombre_nivel+"</td>"
+
+    		datos+="</tr>";
+    		$("#resultados").append(datos)
+		console.log(data)
 	}
-
-	}).success(function(data){
-
-	})
+	});
 })
 /* $("#dictamen").change(function() {
 	$.ajax({
