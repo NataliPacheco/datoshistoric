605847bd013b139cf7da077b3a0df856618b2ef1
NataliPacheco <lnatali_pacheco@hotmail.com>
2018-01-07 18:20:07 -0600

    Agregue consulta en filtros php

--- php/filtro.php
+++ php/filtro.php
@@ -58,7 +58,13 @@ if ("and" == array_slice(explode(" ",$agregar,-1), -1)[0]) {
 }
 //echo $agregar."\n";
 
-$consulta=mysql_query("SELECT * FROM programas WHERE $agregar",$con) or die ("No se puede realizar la consulta");
+$consulta=mysql_query("SELECT programas.num_referencia,programas.nombre_programa,cat_convocatoria.nombre_convocatoria,cat_convocatoria.id_convocatoria,cat_evaluadores.nombre_evaluador,cat_evaluadores.cvu_evaluador, comite.fecha_comite,comite.id_comite,cat_dictamen.estatus, cat_dictamen.id_dictamen, cat_nivel.id_nivel,cat_nivel.nombre_nivel,cat_institucion.nombre_institucion,cat_institucion.id_institucion FROM programas 
+    inner join cat_convocatoria  on programas.id_convocatoria = cat_convocatoria.id_convocatoria
+    inner join cat_institucion on programas.id_institucion = cat_institucion.id_institucion
+    inner join cat_evaluadores on programas.cvu_evaluador = cat_evaluadores.cvu_evaluador
+    inner join comite on programas.id_comite = comite.id_comite
+    inner join cat_dictamen on programas.id_dictamen = cat_dictamen.id_dictamen
+    inner join cat_nivel on programas.id_nivel = cat_nivel.id_nivel order by id_programa  LIMIT $empieza, $por_pagina WHERE $agregar",$con) or die ("No se puede realizar la consulta");
 
 while($row=mysql_fetch_assoc($consulta)) {
 
